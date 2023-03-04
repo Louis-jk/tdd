9632a64dd0e135a531e6cb1c3c0362ccd9810672
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-04 20:21:17 +0900

    modify origin code

--- src/sumClamp.ts
+++ src/sumClamp.ts
@@ -1,11 +1,10 @@
 export function sumClamp(numbers: number[], clamp: number): number {
-  let sum: number = 0;
-  if (numbers && numbers.length > 0) {
-    sum = numbers.reduce((prev: number, curr: number) => {
-      return prev + curr;
-    }, 0);
-  }
+  let result: number = 0;
 
-  let result = sum + clamp;
+  let sum: number = numbers.reduce((prev: number, curr: number) => {
+    return prev + curr;
+  }, 0);
+
+  result = sum + clamp;
   return result;
 }
