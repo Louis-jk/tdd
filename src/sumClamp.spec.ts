ed9db99c69946199f4ef26f18cdafce957b8130d
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-04 20:18:49 +0900

    modify test code and original code and testing

--- src/sumClamp.spec.ts
+++ src/sumClamp.spec.ts
@@ -2,10 +2,14 @@ import { sumClamp } from './sumClamp';
 
 describe('sumClamp', () => {
   it('should sum up to zero', () => {
-    expect(sumClamp([], 10)).toBe(0);
+    expect(sumClamp([], 10)).toBe(10);
   });
 
   it('should sum up to zero from some numbers', () => {
-    expect(sumClamp([-1, 1], 10)).toBe(0);
+    expect(sumClamp([-2, 1], 10)).toBe(9);
+  });
+
+  it('should some some numbers', () => {
+    expect(sumClamp([1, 2, 3, 4], 20)).toBe(30);
   });
 });
--- src/sumClamp.ts
+++ src/sumClamp.ts
@@ -1,3 +1,11 @@
 export function sumClamp(numbers: number[], clamp: number): number {
-  return 0;
+  let sum: number = 0;
+  if (numbers && numbers.length > 0) {
+    sum = numbers.reduce((prev: number, curr: number) => {
+      return prev + curr;
+    }, 0);
+  }
+
+  let result = sum + clamp;
+  return result;
 }
